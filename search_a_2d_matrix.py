cacdf84737b5dca5ffb37ae1f5cf54a2bbc054af
baz <bazil.khan@urbaneyes.ai>
2023-08-10 00:15:53 -0400

    adds search 2d matrix sol

---
+++ search_a_2d_matrix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for row in matrix:
+            for i in row:
+                if i == target:
+                    return True;
+        return False;
