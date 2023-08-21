19ab1e971c1f2f4b524547c4e13bb938dd01b161
baz <bazil.khan@urbaneyes.ai>
2023-08-21 16:58:45 -0400

    adds search in rotated array sol

---
+++ search_in_rotated_sorted_array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i, num in enumerate(nums):
+            if num == target:
+                return i
+        return -1
